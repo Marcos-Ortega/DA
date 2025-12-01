a96b1ca481f6c73a95cfb81f0c1e132b299de0c4
Marcos-Ortega <marcosortega977@gmail.com>
2025-11-30 21:50:13 -0300

    resuelvo error

--- testPrograma.java
+++ testPrograma.java
@@ -559,107 +559,7 @@ public class testPrograma {
 
         return matVuelo;
     }
-<<<<<<< HEAD
-
-    public static List<Vuelo> mergeSort(List<Vuelo> arregloVuelo, List<Ruta> ruta) {
-        int mitad;
-        List<Vuelo> listaVuelo = new ArrayList<>();
-        if (arregloVuelo.size() > 1) {
-            mitad = arregloVuelo.size() / 2;
-            List<Vuelo> arregloIzquierdo = new ArrayList<>(arregloVuelo.subList(0, mitad));// creo arreglo izq
-                                                                                           // divindiendo el origianl
-            List<Vuelo> arregloDerecho = new ArrayList<>(arregloVuelo.subList(mitad, arregloVuelo.size()));// creo
-                                                                                                           // arreglo
-                                                                                                           // derecho
-                                                                                                           // divindiendo
-                                                                                                           // el
-                                                                                                           // origianl
-            arregloIzquierdo = mergeSort(arregloIzquierdo, ruta);
-            arregloDerecho = mergeSort(arregloDerecho, ruta);
-            listaVuelo = merge(arregloIzquierdo, arregloDerecho, ruta);
-        } else {
-            listaVuelo = arregloVuelo;
-        }
-        return listaVuelo;
-    }
-
-    public static List<Vuelo> merge(List<Vuelo> arregloIzquierdo, List<Vuelo> arregloDerecho, List<Ruta> ruta) {
-        List<Vuelo> aux = new ArrayList<>();
-        int i = 0, j = 0;
-        Vuelo vuelo = arregloIzquierdo.get(i), vuelo2 = arregloDerecho.get(j);
-        ;
-        double distancia1, distancia2;
-        // mientras haya elementos en ambas listas, repito
-        while ((i < arregloIzquierdo.size()) && (j < arregloDerecho.size())) {
-            vuelo = arregloIzquierdo.get(i);
-            vuelo2 = arregloDerecho.get(j);
-            distancia1 = distanciaVuelo(vuelo.getIdRuta(), ruta);
-            distancia2 = distanciaVuelo(vuelo2.getIdRuta(), ruta);
-            if (distancia1 <= distancia2) { // comparo las distancias
-                aux.add(vuelo);
-                i++;
-            } else {
-                aux.add(vuelo2);
-                j++;
-            }
-        }
-        // copio elementos que quedaron sin comparar en la primera lista
-        while (i < arregloIzquierdo.size()) {
-            aux.add(arregloIzquierdo.get(i));
-            i++;
-        }
-        // copio elementos que quedaron sin comparar en la segunda lista
-        while (j < arregloDerecho.size()) {
-            aux.add(arregloDerecho.get(j));
-            j++;
-        }
-        return aux;
-    }
-
-    public static void mostrarVuelosOrdenados(Vuelo[][] matVuelo, List<Ruta> ruta, String dia) {
-        int fila = DiaAFila(dia);
-        double km;
-        List<Vuelo> lista = new ArrayList<>();
-        if (fila >= 0 && fila <= 6) {
-            for (int i = 0; i < matVuelo.length; i++) {
-                Vuelo v = matVuelo[fila][i];
-                if (v != null) {
-                    lista.add(v);
-                }
-            }
-            if (!(lista.isEmpty())) {
-                lista = mergeSort(lista, ruta);
-                guardarArchivo(lista, dia);
-                System.out.println("Vuelos Ordenados: ");
-                for (Vuelo v : lista) {
-                    km = distanciaVuelo(v.getIdRuta(), ruta);
-                    System.out.println("Vuelo: " + v.getNroVuelo() +
-                            " | Avion: " + v.getIdAvion() +
-                            " | Ruta: " + v.getIdRuta() +
-                            " | Dia: " + v.getDia() +
-                            " | Hora: " + v.getHora() +
-                            " | KM: " + km);
-                }
-            }
-        }
-    }
-
-    private static void guardarArchivo(List<Vuelo> vuelo, String dia) {
-        try {
-            String nombreArchivo = "VuelosOrdenados-" + dia + ".txt";
-            FileWriter archivo = new FileWriter("C:\\Users\\Marcos\\OneDrive\\Escritorio\\DA\\" + nombreArchivo, true);
-            BufferedWriter escritor = new BufferedWriter(archivo);
-            for (Vuelo v : vuelo) {
-                escritor.write(v.getNroVuelo() + ";" + v.getIdAvion() + ";" + v.getIdRuta() + ";" + v.getDia() + ";"
-                        + v.getHora());
-                escritor.newLine();
-            }
-            escritor.close();
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-
+    
     // modulo para calcular recursivamente horarios sin vuelos
     public static int horarioSinVuelosRec(Vuelo[][] matVuelo, int i, int j) {
         int cantidad = 0;
@@ -677,6 +577,4 @@ public class testPrograma {
         }
         return cantidad;
     }
-=======
->>>>>>> 120030a5f2c787b66d9e518cf2d4245b3a15216a
 }
